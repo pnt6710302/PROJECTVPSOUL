b00fc15b4de90e19f86f73bf865b91ef56cceafe
pnt6710302 <nguyentuanphong0302@gmail.com>
2025-10-01 17:13:38 +0700

    Add files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { FaHome, FaSmile, FaCalendarAlt, FaHeadphones, FaRobot, FaBook, FaCog, FaComment } from "react-icons/fa";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="app-container">
+      {/* Sidebar */}
+      <aside className="sidebar">
+        <h2 className="logo"> VP Soul</h2>
+        <nav>
+          <ul>
+            <li><FaHome /> Dashboard</li>
+            <li><FaRobot /> Tâm Trạng</li>
+            <li><FaComment /> Trò chuyện</li>
+            <li><FaBook /> Nhật kí</li>
+            <li><FaCalendarAlt /> Lịch Trình</li>
+            <li><FaHeadphones /> Thư Giãn</li>
+            <li><FaCog /> Setting</li>
+          </ul>
+        </nav>
+      </aside>
+
       
-      </header>
     </div>
   );
 }
