b77dbffe535def5b8305d9d19e27ce57fbb5695e
nav619 <avn16112009@gmail.com>
2025-10-18 14:50:11 +0700

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
-import React from "react";
-import "./App.css";
-import { FaHome, FaSmile, FaCalendarAlt, FaHeadphones, FaRobot, FaBook, FaCog, FaComment } from "react-icons/fa";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <div className="app-container">
-      {/* Sidebar */}
-      <aside className="sidebar">
-        <h2 className="logo"> VP Soul</h2>
-        <nav>
-          <ul>
-            <li><FaHome /> Dashboard</li>
-            <li><FaRobot /> Tâm Trạng</li>
-            <li><FaComment /> Trò chuyện</li>
-            <li><FaBook /> Nhật kí</li>
-            <li><FaCalendarAlt /> Lịch Trình</li>
-            <li><FaHeadphones /> Thư Giãn</li>
-            <li><FaCog /> Cài Đặt</li>
-          </ul>
-        </nav>
-      </aside>
-
+    <div className="App">
+      <header className="App-header">
       
+      </header>
     </div>
   );
 }
